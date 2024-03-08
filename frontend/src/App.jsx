b6a2c2ff1f36ccfc4b4f7b764d166166dfe10331
Murat Seçgin <murat.scgn@gmail.com>
2024-03-08 18:24:18 +0300

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,11 @@ import { Header } from './components/Layout/Header/Header'
 import Policy from './components/Layout/Policy/Policy'
 import Products from './components/Products/Products'
 import Slider from './components/Slider/Slider'
+import Campaigns from './components/Campaigns/Campaigns'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Brands from './components/Brands/Brands'
+import CampaignSingle from './components/CampaignSingle/CampaignSingle'
 
 function App() {
 
@@ -14,6 +18,11 @@ function App() {
       <Slider></Slider>
       <Categories></Categories>
       <Products></Products>
+      <Campaigns></Campaigns>
+      <Products></Products>
+      <Blogs></Blogs>
+      <Brands></Brands>
+      <CampaignSingle></CampaignSingle>
       <Policy></Policy>
       <Footer></Footer>
     </>
