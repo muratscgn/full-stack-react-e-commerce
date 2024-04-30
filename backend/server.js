6f79a40479f742ea5fc5baea7ff818aaa132529f
Murat Seçgin <murat.scgn@gmail.com>
2024-04-30 14:49:45 +0300

    add backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello Express.js");
+})
+
+app.get("/api", (req, res) => {
+  res.send("This is API Route");
+})
+
+app.listen(5000, () => {
+  console.log(`Sunucu ${5000} portunda çalışıyor.`)
+})
