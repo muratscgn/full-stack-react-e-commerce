f0b9da5984a098db8b6dd40a039eb0c3d2461408
Murat Seçgin <murat.scgn@gmail.com>
2024-05-01 12:10:37 +0300

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const app = express()
+const mainRoute = require("./routes/index")
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express.js");
-})
 
-app.get("/api", (req, res) => {
-  res.send("This is API Route");
-})
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to mongoDb")
+  } catch (error) {
+    throw error
+  }
+}
+
+app.use("/api", mainRoute)
 
 app.listen(5000, () => {
+  connect()
   console.log(`Sunucu ${5000} portunda çalışıyor.`)
 })
